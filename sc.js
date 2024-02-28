38bb94801d792752fd2789a5921b05af08ad2242
natzsixn <129019659+natzsixn@users.noreply.github.com>
2024-02-28 21:35:34 +0700

    Add files via upload
    ==> Perkenalkan saya ntah lah yang penting udah ke up github hehe:D

---
+++ sc.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const seconds = document.querySelector(".seconds .number"),
+    minutes = document.querySelector(".minutes .number"),
+    hours = document.querySelector(".hours .number"),
+    days = document.querySelector(".days .number");
+  let secValue = 11,
+    minValue = 2,
+    hourValue = 2,
+    dayValue = 40;
+  const timeFunction = setInterval(() => {
+    secValue--;
+    if (secValue === 0) {
+      minValue--;
+      secValue = 60;
+    }
+    if (minValue === 0) {
+      hourValue--;
+      minValue = 60;
+    }
+    if (hourValue === 0) {
+      dayValue--;
+      hourValue = 24;
+    }
+    if (dayValue === 0) {
+      clearInterval(timeFunction);
+    }
+    seconds.textContent = secValue < 10 ? `0${secValue}` : secValue;
+    minutes.textContent = minValue < 10 ? `0${minValue}` : minValue;
+    hours.textContent = hourValue < 10 ? `0${hourValue}` : hourValue;
+    days.textContent = dayValue < 10 ? `0${dayValue}` : dayValue;
+  }, 1000); //1000ms = 1
+
+  var keteranganCarousels = document.querySelectorAll(".second");
+
+  var displayCharacters = 120;
+
+  keteranganCarousels.forEach(function (element) {
+    var originalText = element.textContent;
+
+    var trimmedText = originalText.slice(0, displayCharacters);
+    var displayText =
+      trimmedText + (originalText.length > displayCharacters ? "..." : "");
+
+    // Update the text content of the element
+    element.textContent = displayText;
+  });
+});
